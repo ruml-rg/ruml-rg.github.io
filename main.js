1591b0f9e45fa9f6d374d5f76716226b5eae1928
cannon <cannon10100@gmail.com>
2016-10-22 16:04:07 -0500

    Added reading list data

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
-var app = new Vue({
-	el: '#app',
-  data: {
-  	todos: [
-    	{text: "Hello"},
-      {text: "Goodbye"},
-      {text: "See you around"}
-    ]
-  }
+var current_pointer = 1
+var future_pointer = 5
+
+$(function(){
+  $.get('/roadmap_mod.txt', function(data){
+      var papers = data.split(';');
+      var past_papers = [];
+      var current_papers = [];
+      var future_papers = [];
+
+      for (idx in papers) {
+        if (idx < current_pointer) {
+          past_papers.push(JSON.parse(papers[idx]));
+        } else if (idx < future_pointer) {
+          current_papers.push(JSON.parse(papers[idx]));
+        } else {
+          future_papers.push(JSON.parse(papers[idx]));
+        }
+      }
+
+      console.log(current_papers);
+
+      var app_past = new Vue({
+      	el: '#app-past',
+        data: {
+          papers: past_papers
+        }
+      });
+
+      var app_current = new Vue({
+      	el: '#app-current',
+        data: {
+          papers: current_papers
+        }
+      });
+
+      var app_future = new Vue({
+      	el: '#app-future',
+        data: {
+          papers: future_papers
+        }
+      });
+  });
 });
